1968b308a9f8e00afd342e0c80692bf91f88f902
gollol <s8faselz@stud.uni-saarland.de>
2024-09-05 20:09:29 +0200

    modified xml files to match parser and added basic parsing into main

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -4,14 +4,25 @@
 
 int main() {
 
-    // tinyxml2::XMLDocument gameDoc;
-    // tinyxml2::XMLError error = gameDoc.LoadFile("resources/game.xml");
+    tinyxml2::XMLDocument gameDoc;
+    tinyxml2::XMLError gameError = gameDoc.LoadFile("resources/game.xml");
+    if (gameError != tinyxml2::XML_SUCCESS) return 1;
 
-    // if (error != tinyxml2::XML_SUCCESS) return 1;
+    tinyxml2::XMLNode* gameRoot = gameDoc.FirstChild();
+    if (gameRoot == nullptr) return 1;
 
-    // tinyxml2::XMLNode* root = gameDoc.FirstChild();
-    // if (root == nullptr) return false;
+    tinyxml2::XMLNode* mapPath = gameRoot->FirstChildElement("scene");
+    if (mapPath == nullptr) return 1;
 
+    // TODO: Read SPAWNER locations from gameDoc
+
+    auto sceneFileName = ("resources/" + std::string(mapPath->FirstChild()->Value())).c_str();
+    tinyxml2::XMLDocument sceneDoc;
+    tinyxml2::XMLError sceneError = sceneDoc.LoadFile(sceneFileName);
+    if (sceneError != tinyxml2::XML_SUCCESS) return 1;
+
+    tinyxml2::XMLNode* sceneRoot = sceneDoc.FirstChild();
+    if (sceneRoot == nullptr) return 1;
 
     sf::GameEngine engine(42);
     engine.execute();
