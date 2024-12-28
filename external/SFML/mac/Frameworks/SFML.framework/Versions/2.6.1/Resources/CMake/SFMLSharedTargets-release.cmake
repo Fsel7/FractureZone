90645e77a8d84a7423b6d8c6ae3f37d348dc1664
gollol <s8faselz@stud.uni-saarland.de>
2024-12-28 20:59:58 +0100

    SHOULD work now

--- external/SFML/mac/Frameworks/SFML.framework/Versions/2.6.1/Resources/CMake/SFMLSharedTargets-release.cmake
+++ external/SFML/mac/Frameworks/SFML.framework/Versions/2.6.1/Resources/CMake/SFMLSharedTargets-release.cmake
@@ -5,59 +5,59 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Paths are off for some reason
-get_filename_component(PARENT_DIR ${_IMPORT_PREFIX} DIRECTORY)
-get_filename_component(GRANDPARENT_DIR ${PARENT_DIR} DIRECTORY)
+# # Paths are off for some reason
+# get_filename_component(PARENT_DIR ${_IMPORT_PREFIX} DIRECTORY)
+# get_filename_component(GRANDPARENT_DIR ${PARENT_DIR} DIRECTORY)
 
 # Import target "sfml-system" for configuration "Release"
 set_property(TARGET sfml-system APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(sfml-system PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${GRANDPARENT_DIR}/sfml-system.framework/Versions/2.6.1/sfml-system"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sfml-system.framework/Versions/2.6.1/sfml-system"
   IMPORTED_SONAME_RELEASE "@rpath/sfml-system.framework/Versions/2.6.1/sfml-system"
 )
 
 list(APPEND _cmake_import_check_targets sfml-system )
-list(APPEND _cmake_import_check_files_for_sfml-system "${GRANDPARENT_DIR}/sfml-system.framework/Versions/2.6.1/sfml-system" )
+list(APPEND _cmake_import_check_files_for_sfml-system "${_IMPORT_PREFIX}/sfml-system.framework/Versions/2.6.1/sfml-system" )
 
 # Import target "sfml-window" for configuration "Release"
 set_property(TARGET sfml-window APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(sfml-window PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${GRANDPARENT_DIR}/sfml-window.framework/Versions/2.6.1/sfml-window"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sfml-window.framework/Versions/2.6.1/sfml-window"
   IMPORTED_SONAME_RELEASE "@rpath/sfml-window.framework/Versions/2.6.1/sfml-window"
 )
 
 list(APPEND _cmake_import_check_targets sfml-window )
-list(APPEND _cmake_import_check_files_for_sfml-window "${GRANDPARENT_DIR}/sfml-window.framework/Versions/2.6.1/sfml-window" )
+list(APPEND _cmake_import_check_files_for_sfml-window "${_IMPORT_PREFIX}/sfml-window.framework/Versions/2.6.1/sfml-window" )
 
 # Import target "sfml-network" for configuration "Release"
 set_property(TARGET sfml-network APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(sfml-network PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${GRANDPARENT_DIR}/sfml-network.framework/Versions/2.6.1/sfml-network"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sfml-network.framework/Versions/2.6.1/sfml-network"
   IMPORTED_SONAME_RELEASE "@rpath/sfml-network.framework/Versions/2.6.1/sfml-network"
 )
 
 list(APPEND _cmake_import_check_targets sfml-network )
-list(APPEND _cmake_import_check_files_for_sfml-network "${GRANDPARENT_DIR}/sfml-network.framework/Versions/2.6.1/sfml-network" )
+list(APPEND _cmake_import_check_files_for_sfml-network "${_IMPORT_PREFIX}/sfml-network.framework/Versions/2.6.1/sfml-network" )
 
 # Import target "sfml-graphics" for configuration "Release"
 set_property(TARGET sfml-graphics APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(sfml-graphics PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${GRANDPARENT_DIR}/sfml-graphics.framework/Versions/2.6.1/sfml-graphics"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sfml-graphics.framework/Versions/2.6.1/sfml-graphics"
   IMPORTED_SONAME_RELEASE "@rpath/sfml-graphics.framework/Versions/2.6.1/sfml-graphics"
 )
 
 list(APPEND _cmake_import_check_targets sfml-graphics )
-list(APPEND _cmake_import_check_files_for_sfml-graphics "${GRANDPARENT_DIR}/sfml-graphics.framework/Versions/2.6.1/sfml-graphics" )
+list(APPEND _cmake_import_check_files_for_sfml-graphics "${_IMPORT_PREFIX}/sfml-graphics.framework/Versions/2.6.1/sfml-graphics" )
 
 # Import target "sfml-audio" for configuration "Release"
 set_property(TARGET sfml-audio APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(sfml-audio PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${GRANDPARENT_DIR}/sfml-audio.framework/Versions/2.6.1/sfml-audio"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/sfml-audio.framework/Versions/2.6.1/sfml-audio"
   IMPORTED_SONAME_RELEASE "@rpath/sfml-audio.framework/Versions/2.6.1/sfml-audio"
 )
 
 list(APPEND _cmake_import_check_targets sfml-audio )
-list(APPEND _cmake_import_check_files_for_sfml-audio "${GRANDPARENT_DIR}/sfml-audio.framework/Versions/2.6.1/sfml-audio" )
+list(APPEND _cmake_import_check_files_for_sfml-audio "${_IMPORT_PREFIX}/sfml-audio.framework/Versions/2.6.1/sfml-audio" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
